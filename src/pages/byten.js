9a6598025d0d9c451e7fefedeae67cfd789f9b7e
Jacqueline Wettergren <jacqueline_wettergren@hotmail.com>
2021-03-19 11:43:15 +0100

    2103171540

--- src/pages/byten.js
+++ src/pages/byten.js
@@ -27,9 +27,11 @@ const Byten = () => {
             <ol>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-
+                        <li>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
                     )
-
                 })}
             </ol>
         </Layout>
